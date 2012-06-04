54ee30753523115297456c0d1804c67a08d357b9
fabio <pasquale.gatto@me.com>
2012-06-04 21:04:58 +0100

    commit git comment 2

--- src/test/scala/code/xmlmessages/XmlMessageUtilSpec.scala
+++ src/test/scala/code/xmlmessages/XmlMessageUtilSpec.scala
@@ -16,7 +16,7 @@ class XmlMessageUtilSpec extends SpecificationWithJUnit with WebSpecification {
     }
 
     "the names " in {
-      XmlMessageUtil.getProdDetails(file)    // git
+      XmlMessageUtil.getProdDetails(file)    // git comment test
       true
     }
   }
