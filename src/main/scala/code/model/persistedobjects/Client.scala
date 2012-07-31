e3a88050e3fb195265c3935d870e591b21834042
fabio <pasquale.gatto@me.com>
2012-07-31 19:13:37 +0100

    method of validation changed. MappedObj validation

--- src/main/scala/code/model/persistedobjects/Client.scala
+++ src/main/scala/code/model/persistedobjects/Client.scala
@@ -3,36 +3,35 @@ package code.model.persistedobjects
 import net.liftweb.mapper._
 
 class Client extends LongKeyedMapper[Client] with CreatedUpdated with IdPK {
-
   def getSingleton = Client
-  object forenames extends MappedString(this, 255)
-  object surname extends MappedString(this, 255)
+
+  object forenames extends MappedString(this, 255) {
+    override def validations = valMinLen(1, {"Name too short, dude!"}) _ :: super.validations
+  }
+
+  object surname extends MappedString(this, 255) {
+//    override def validations = valMinLen(1, {"surname too short, dude!"}) _ :: super.validations
+  }
   object email extends MappedString(this, 255)
   object password extends MappedString(this, 255)
-  object securityAnswer extends MappedString(this, 255)
   object address extends MappedString(this, 255)
   object postCode extends MappedString(this, 255)
   object country extends MappedString(this, 255)
-  object heardAboutUs extends MappedString(this, 255)
-
 }
 
 object Client extends Client with LongKeyedMetaMapper[Client] {
 
   def apply(forenames: String, surname: String, email: String, password: String,
-            securityAnswer: String, address: String, postCode: String, country: String, heardAboutUs: String) = {
+              address: String, postCode: String, country: String) = {
     Client.create
       .forenames(forenames)
       .surname(surname)
       .email(email)
       .password(password)
-      .securityAnswer(securityAnswer)
       .address(address)
       .postCode(postCode)
       .country(country)
-      .heardAboutUs(heardAboutUs)
   }
-
   override def dbTableName = "clients"
 }
 
--- src/main/scala/code/snippet/RegistrationForm.scala
+++ src/main/scala/code/snippet/RegistrationForm.scala
@@ -7,6 +7,7 @@ import net.liftweb.http.S
 import net.liftweb.http.js.JsCmds
 import xml.Text
 import scala.collection.mutable.Map
+import net.liftweb.util.FieldError
 
 object RegistrationForm {
   def render = {
@@ -14,65 +15,37 @@ object RegistrationForm {
     var surname = ""
     var email = ""
     var password = ""
-    var securityAnswer = ""
     var address = ""
     var postCode = ""
     var country = ""
-    var heardAboutUs = ""
-    var fieldsValidation: Map[String, Seq[((String) => Boolean, String)]] = Map()
-
-    val cannotBeEmpty = (empty _, "Cannot be empty")
-    val cannotBeTooShort = (validateLength _, "must be minimum 2 characters long")
-    val passLength = (passwordLength _, "Password must be minimum 4 characters long")
-    val emailForm = (emailFormat _, "The email is not the right format")
-
-    fields.map(f =>
-      f match {
-        case "email" => fieldsValidation += ("email" -> Seq(cannotBeEmpty, cannotBeTooShort, emailForm))
-        case "password" => fieldsValidation += ("password" -> Seq(passLength, cannotBeEmpty))
-        case "forenames" => fieldsValidation += ("forenames" -> Seq(cannotBeEmpty))
-        case field: String => fieldsValidation += (field  -> Seq(cannotBeEmpty))
-      })
-
-    def generateErrorMsg(field: String) = JsCmds.SetHtml("forenamesError", Text(field + " error"))
 
     def process() = {
-      if (forenames.equals("")) {
-        JsCmds.SetHtml("forenamesError", Text(" error"))
-//      var bool: Boolean = false
-//      var field: String = ""
-//      fieldsValidation.keys.map(f => fieldsValidation.values.map(x => x.map(y => if (y._1(f)) {
-//        bool = true; field = f
-//      })))
-//      if (bool) {
-//        generateErrorMsg(field)
+      val client = Client(forenames, surname, email, password, address, postCode, country)
+      if(client.validate.length > 0) {
+        client.validate.foreach(e => println("e " + e.msg + " " + e.field))
+        JsCmds.SetHtml("forenamesError", Text("bbb"))
       } else {
-        val client = Client(forenames, surname, email, password, securityAnswer, address, postCode, country, heardAboutUs)
-        client.save()
-        S.redirectTo("/index.html")
+        client.save
+        println(client.validate.length + " invoked")
+        JsCmds.Noop
       }
+//          client.validate match {
+//            case Nil => client.save(); S.redirectTo("/index.html")
+//            case error: List[FieldError] => JsCmds.SetHtml("forenamesError", Text("llllllll")); println(error.length + " invoked")
+//        }
+//      def test = JsCmds.SetHtml("forenamesError", Text("llllllll"))
     }
 
     "#forenames" #> text(forenames, forenames = _ , "id" -> "forenames") &
     "#surname" #> text(surname, surname = _ , "id" -> "surname") &
     "#email" #> text(email, email = _ , "id" -> "email") &
     "#password" #> text(password, password = _ , "id" -> "password") &
-    "#securityAnswer" #> text(securityAnswer, securityAnswer = _ , "id" -> "securityAnswer") &
     "#address" #> text(address, address = _ , "id" -> "address") &
     "#postCode" #> text(postCode, postCode = _ , "id" -> "postCode") &
     "#country" #> text(country, country = _ , "id" -> "country") &
-    "#heardAboutUs" #> text(heardAboutUs, heardAboutUs = _ , "id" -> "heardAboutUs") &
     "#submit" #>  ajaxSubmit("Register", () => {
       process()
     })
   }
-
-  private def empty(field: String) = field.length == 0
-  private def validateLength(field: String) = field.length < 2
-  private def passwordLength(password: String) = password.length < 4
-  private def emailFormat(email: String) = !email.contains("@")
-
-
-  val fields = List("forenames", "surname", "email", "password", "securityAnswer", "address", "postCode", "country", "hearAboutUs")
 }
 
