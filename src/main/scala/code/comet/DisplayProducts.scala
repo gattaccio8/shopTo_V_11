f09a34c5cc27ae58a7320fad807e52b65baf6aca
fabio <pasquale.gatto@me.com>
2012-11-21 20:38:44 +0000

    replace cssSelector with NodeSeq

--- src/main/scala/code/comet/DisplayProducts.scala
+++ src/main/scala/code/comet/DisplayProducts.scala
@@ -1,22 +1,34 @@
 package code.comet
 
-import net.liftweb.util.Helpers
 import code.model.xmlmessages.XmlMessageUtil
 import java.io.File
 import net.liftweb.http.CometActor
 
 class DisplayProducts extends CometActor {
 
-   def render = {
-    import Helpers._
+//   def render = {
+//    import Helpers._
+//
+//    val file = new File("/Users/fabio/Documents/dev/projects/net.shopto/src/test/resources/response/response.xml")
+////    val products = XmlMessageUtil.getProdDetails //file)
+//    val products = XmlMessageUtil.getProdDetails(file)
+//     ".products *" #> products.map(x => ".td1 *" #> <img class="images" src={x.image} alt="picture"/> &
+//     ".td2 *" #>  x.name &
+//     ".td3 *" #> (<div>£{x.price} {x.currency}</div>) )
+//   }
 
+  override def render = {
     val file = new File("/Users/fabio/Documents/dev/projects/net.shopto/src/test/resources/response/response.xml")
-//    val products = XmlMessageUtil.getProdDetails //file)
     val products = XmlMessageUtil.getProdDetails(file)
-     ".products *" #> products.map(x => ".td1 *" #> <img class="images" src={x.image} alt="picture"/> &
-     ".td2 *" #>  x.name &
-     ".td3 *" #> (<div>£{x.price} {x.currency}</div>) )
-   }
+    <div class="row-fluid">
+      { products.map(x =>
+        <div> { <img class="images" src={x.image} alt="picture"/> } </div>
+        <div class="gamename"> { x.name } </div>
+        <div class="price"> { <p>£{ x.price } { x.currency }</p> } </div>)
+      }
+    </div>
+
+  }
 
 
   override def localSetup {
--- src/test/scala/code/snippet/DisplayProductsSpec.scala
+++ src/test/scala/code/snippet/DisplayProductsSpec.scala
@@ -8,18 +8,18 @@ class DisplayProductsSpec extends SpecificationWithJUnit with IndexPageDriver {
   "index page" should {
     "display the products list" in {
       assertIsHomePage
-      firefoxDriver.findElementByClassName("products").isDisplayed
+      firefoxDriver.findElementByClassName("images").isDisplayed
     }
 
     "display the items list" in {
-      val bool = firefoxDriver.findElementByClassName("td2").getText.equals("One Piece Gigant Battle")
-      println(" result = " + firefoxDriver.findElementByClassName("td2").getText)
+      val bool = firefoxDriver.findElementByClassName("gamename").getText.equals("One Piece Gigant Battle")
+      println(" result = " + firefoxDriver.findElementByClassName("gamename").getText)
       bool
     }
 
     "display the item price" in {
-      val bool = firefoxDriver.findElementByClassName("td3").getText.equals("£35.79 GBP")
-      println(" result = " + firefoxDriver.findElementByClassName("td3").getText)
+      val bool = firefoxDriver.findElementByClassName("price").getText.equals("£35.79 GBP")
+      println(" result = " + firefoxDriver.findElementByClassName("price").getText)
       bool
     }
   }
