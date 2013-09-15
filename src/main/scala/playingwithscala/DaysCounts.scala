8990ca0f6aae885740b94bddb00a8450b16b9378
fabio <pasquale.gatto@me.com>
2013-09-15 20:52:30 +0100

    adding a function to the typeclass

--- src/main/scala/playingwithscala/DaysCounts.scala
+++ src/main/scala/playingwithscala/DaysCounts.scala
@@ -5,28 +5,26 @@ import org.joda.time.LocalDate
 
 case class Dates(d1: LocalDate, d2: LocalDate)
 
-trait Counter[T, R] {
+trait DaysCount[T, R] {
   def counting(t: T): R
+  def countingMonths(t: T): R
 }
 
-object Counter {
-  implicit object DaysCounter extends Counter[Dates, Int] {
+object DaysCount {
+  implicit object DatesDaysCount extends DaysCount[Dates, Int] {
     var days = 0
-    def counting(t: Dates): Int =
-      if(!t.d1.equals(t.d2)) {
-        days = days + 1
-        counting(Dates(t.d1.plusDays(1), t.d2))
-      } else
-        days
+    def counting(t: Dates): Int = if(!t.d1.equals(t.d2)) { days = days + 1; counting(Dates(t.d1.plusDays(1), t.d2)) } else days
+    def countingMonths(t: Dates): Int = counting(t) / 30
   }
 }
 
-
 trait Count {
-  def counting[T, R](t: T)(implicit daysCount: Counter[T, R]): R = daysCount.counting(t)
+  def counting[T, R](t: T)(implicit daysCount: DaysCount[T, R]): R = daysCount.counting(t)
+  def countingMonths[T, R](t: T)(implicit daysCount: DaysCount[T, R]): R = daysCount.countingMonths(t)
 }
 
 
 object ExampleWithDefaults extends App with Count {
   println(counting(Dates(new LocalDate(2013, 9, 1), new LocalDate(2013, 9, 15))))
+  println(countingMonths(Dates(new LocalDate(2013, 9, 1), new LocalDate(2014, 9, 15))))
 }
